abf14fa8d996bc58f517ba77bafdcd0153d9e63b
marogers <marogers@yammer-inc.com>
2011-06-22 14:25:34 -0700

    yammer wrapper, plus stringify function

--- parseuri.js
+++ parseuri.js
@@ -1,12 +1,19 @@
+;(function(yam, undefined) {
+
+// This is the yammer fork of parseuri.js.
+// Any changes MUST BE IMMEDIATELY pushed back upstream, as this will be
+// copy pasted over.
+// To prevent jsmin from removing copyright, /*! */ is added.
+// =============== BEGIN parseUri =========================
+
+/*!
 // a node.js module fork of
 // parseUri 1.2.2
 // (c) Steven Levithan <stevenlevithan.com>
 // MIT License
 // see: http://blog.stevenlevithan.com/archives/parseuri
 // see: http://stevenlevithan.com/demo/parseuri/js/
-
-//forked into a node.js module by franz enzenhofer
- 
+*/
 
 function parseUri (str) {
 	var	o   = parseUri.options,
@@ -37,4 +44,35 @@ parseUri.options = {
 	}
 };
 
-module.exports = parseUri
+// =========== END parseUri ===========
+
+yam.ns('yam.uri');
+
+/**
+ * Parse a URI string into an object with keys representing the relevant parts.
+ * See parseUri
+ * @param String The uri to parse
+ */
+yam.uri.parse = parseUri
+
+/**
+ * Join the parts of an object created by yam.uri.parse into a valid uri. This is
+ * not guaranteed to work with custom objects. Also, if you have not modified the
+ * parsed object, just use uriObj.source which contains the original string.
+ * @param Object an object returned from yam.uri.parse
+ */
+yam.uri.stringify = function(uriObj) {
+  var keys = parseUri.options.key
+      uri = '';
+
+  for(var i = 0, len = keys.length; i < len; i++) {
+    // For the query, use the queryKey object because it may have been modified
+    if(keys[i] === 'query') {
+      uri += yam.paramify( uriObj['queryKey'] );
+    } else {
+      uri += uriObj[ keys[i] ];
+    }
+  }
+}
+
+})(yam);
